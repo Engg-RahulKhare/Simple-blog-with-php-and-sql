3dd6ee859829c8ba7b7764f9e15beaeb462d64e9
Engg-RahulKhare <90152262+Engg-RahulKhare@users.noreply.github.com>
2022-06-24 07:44:29 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('APP_ROOT', '/blog');
 define('DEFAULT_CONTROLLER', 'home');
 define('DEFAULT_ACTION', 'index');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'blog');
+define('DB_HOST', '65.21.90.199');
+define('DB_USER', 'rlukceno_technofenatics');
+define('DB_PASS', 'Colour10#');
+define('DB_NAME', 'rlukceno_blog');
